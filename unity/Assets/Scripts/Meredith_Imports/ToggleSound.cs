7c900494a81f5dbafbd720096160c3efaf085ff5
Meredith Baldwin <mcbaldwin@mail.bradley.edu>
2019-03-11 22:01:08 -0700

    Music and Icon

---
+++ unity/Assets/Scripts/Meredith_Imports/ToggleSound.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleSound : MonoBehaviour
+{
+    private bool _isMuted = false;    
+
+    public void ToggledSound()
+    {
+        _isMuted = !_isMuted; // inverts the value. false -> true. true -> false
+        AudioListener.volume = _isMuted ? 1f : 0f; // ? operator allows you to assing values depending on a boolean expression (expression?true:false;)
+    }
+
+}
+
+
