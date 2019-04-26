723e7075a8ad65b559488dce1ad48588d38887a5
Scott Duman <sbd39@comcast.net>
2019-04-26 03:57:58 -0700

    Added Exit Confirm to Main Menu
    Implemented exit confirm popup for main menu as well as implemented updated sound button

---
+++ unity/Assets/Scripts/UI/ExitGamePopup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bradley.AlienArk
+{
+	public class ExitGamePopup : Popup
+	{
+		MainMenu MainMenu;
+
+		// Use this for initialization
+		void Start () 
+		{
+			init();
+			MainMenu = GetComponentInParent<MainMenu>();
+			header.text = "Exit Game?";
+			body.text = "Do you want to exit the game?";
+		}
+
+		protected override void OnYesClicked()
+		{
+			Application.Quit();
+		}
+
+		protected override void OnNoClicked()
+		{
+			MainMenu.ReturnToMenu();
+			Destroy(gameObject);
+		}
+	}
+}
--- unity/Assets/Scripts/UI/MainMenu.cs
+++ unity/Assets/Scripts/UI/MainMenu.cs
@@ -1,26 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 namespace Bradley.AlienArk
 {
     public class MainMenu : MonoBehaviour
     {
-        private void Awake()
+        public GameObject menu;
+        public Button play, sound, exit;
+        private GameObject overlay, exitConfirm;
+        Text soundText;
+
+        private void Start()
         {
             AudioManager.Instance.PlaySound("Background Music");
+            SetSoundButton();
+            SetOverlay();
+            exitConfirm = Resources.Load<GameObject>("UI/ExitConfirmation");
+            play.onClick.AddListener(OnPlayClicked);
+            sound.onClick.AddListener(OnSoundButtonClicked);
+            exit.onClick.AddListener(OnExitClicked);
         }
 
-        public void PlayGame ()
+        private void SetOverlay()
+		{
+			overlay = Instantiate(Resources.Load<GameObject>("UI/Overlay"), transform);
+			overlay.transform.SetSiblingIndex(2);
+			overlay.SetActive(false);
+		}
+
+		private void SetSoundButton()
+		{
+			if (soundText == null) soundText = sound.GetComponentInChildren<Text>();
+			string text = "Sound ";
+			if (AudioListener.volume == 1) text += "On";
+			else text += "Off";
+			soundText.text = text;
+		}
+
+        private void ActivateOverlay(bool value)
+        {
+            menu.SetActive(!value);
+            overlay.SetActive(value);
+        }
+
+        public void OnPlayClicked()
         {
             SceneManager.LoadScene("Level_1");
         }
-        
-        public void QuitGame ()
+
+                public void OnSoundButtonClicked()
         {
-            Debug.Log("Quit!");
-            Application.Quit(); //*Uncomment this if you actually want the game to quit*
+            if (AudioListener.volume == 1) AudioListener.volume = 0;
+            else AudioListener.volume = 1;
+            SetSoundButton();
+        }
+        
+        public void OnExitClicked()
+        {
+            ActivateOverlay(true);
+            Instantiate(exitConfirm, transform);
+        }
+
+        public void EnterMenu()
+        {
+            ActivateOverlay(true);
+        }
+
+        public void ReturnToMenu()
+        {
+            ActivateOverlay(false);
         }
     }
 }
