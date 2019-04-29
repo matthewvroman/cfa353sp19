fbc4b8c87d7c30b4ee31ee738a2b55d65cade878
Scott Duman <sbd39@comcast.net>
2019-04-29 15:26:04 -0700

    Implemented Button Sound Effect
    Implemented the click sound effect so that anytime you click a button with sound on you will hear the button click sound effect.

---
+++ unity/Assets/Scripts/Audio/ButtonClickSFX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Bradley.AlienArk
+{
+	public class ButtonClickSFX : MonoBehaviour 
+	{
+
+		// Use this for initialization
+		void Start () 
+		{
+			GetComponent<Button>().onClick.AddListener(OnClick);
+		}
+
+		private void OnClick()
+		{
+			AudioManager.Instance.PlaySound("Click");
+		}
+	}
+}
